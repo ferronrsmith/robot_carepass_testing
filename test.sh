f46740c10af5c1b45a4489c5d72cdd4cf2586e9d
ferron <ferronrsmith@gmail.com>
2014-02-07 18:57:02 -0400

    added documentation

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 # beta 				-	run currently worked on tests with a beta tag 			:- 	[Tags]	beta
 # qa_only			-	run currently worked on tests with the qa_only tag 		:- 	[Tags]	qa_only
 # prod_only 		-	run currently worked on tests with the prod_only tag 	:- 	[Tags]	prod_only
+# leave params to run all tests
 if [[ $1=='beta' ]]; then
 	pybot -i beta tests/
 elif [[ $1=='qa_only' ]]; then
